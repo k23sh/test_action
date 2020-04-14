5bf03efd39ffcde1e20c4ddcb49f3d97c1539573
강수현 <>
2020-04-14 15:21:27 +0900

    test

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ function test(){
   const ev = JSON.parse(
     fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
   )
-  const prNum = ev.pull_request.number
+  const prNum = ev.pull_request.number;
+  const branche = ev.head.ref;
+  
   console.log('prNum', prNum);
-  console.log('ev', JSON.stringify(ev));
+  console.log('branche', branche);
 }
 test();
